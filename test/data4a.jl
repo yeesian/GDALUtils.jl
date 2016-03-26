14803dbd0ce0441ab9ca6d0818d84bc5ff3647a5
yeesian <ngyeesian@gmail.com>
2016-03-26 18:16:02 -0400

    add more examples

---
+++ test/data4a.jl
@@ -0,0 +1,80 @@
+# adapted from http://www.gis.usu.edu/~chrisg/python/2009/lectures/ospy_hw4a.py
+
+import GDALUtils; const GU = GDALUtils
+
+GU.read("ospy/data4/sites.shp") do shp
+    GU.read("ospy/data4/aster.img") do img
+        shplayer = GU.fetchlayer(shp, 0)
+        featuredefn = GU.getlayerdefn(shplayer)
+        id_index = GU.getfieldindex(featuredefn, "ID")
+
+        # get georeference info
+        transform = GU.getgeotransform(img)
+        xOrigin = transform[1]
+        yOrigin = transform[4]
+        pixelWidth = transform[2]
+        pixelHeight = transform[6]
+
+        # loop through the features in the shapefile
+        for feature in shplayer
+            geom = GU.getgeom(feature)
+            x = GU.getx(geom, 0)
+            y = GU.gety(geom, 0)
+            # compute pixel offset
+            xOffset = round(Int, (x - xOrigin) / pixelWidth)
+            yOffset = round(Int, (y - yOrigin) / pixelHeight)
+            # create a string to print out
+            s = string(GU.fetchfield(feature, id_index)) * " "
+            for j in 1:GU.nraster(img)
+                data = GU.fetch(img, j, 1, 1, xOffset, yOffset)
+                s = s * string(data[1,1]) * " "
+            end
+            # print out the data string
+            println(s)
+        end
+    end
+end
+
+# Expected Output:
+# 1 46 30 47
+# 2 85 77 76
+# 3 64 51 66
+# 4 71 60 73
+# 5 59 40 68
+# 6 97 95 80
+# 7 51 33 52
+# 8 79 69 62
+# 9 78 74 63
+# 10 90 81 67
+# 11 82 81 67
+# 12 100 88 68
+# 13 90 80 65
+# 14 76 65 81
+# 15 83 75 61
+# 16 73 61 63
+# 17 81 71 68
+# 18 69 57 60
+# 19 98 90 75
+# 20 91 79 76
+# 21 86 77 64
+# 22 78 73 63
+# 23 82 66 71
+# 24 64 46 82
+# 25 69 55 68
+# 26 98 88 78
+# 27 62 43 79
+# 28 68 57 62
+# 29 66 51 74
+# 30 60 39 83
+# 31 80 62 80
+# 32 86 73 86
+# 33 97 86 84
+# 34 47 29 49
+# 35 91 74 90
+# 36 67 53 63
+# 37 71 59 63
+# 38 54 23 16
+# 39 53 22 17
+# 40 76 66 61
+# 41 0 0 0
+# 42 0 0 0
---
+++ test/data4b.jl
@@ -0,0 +1,93 @@
+# adapted from http://www.gis.usu.edu/~chrisg/python/2009/lectures/ospy_hw4b.py
+
+import GDALUtils; const GU = GDALUtils
+
+# version 1
+@time GU.read("ospy/data4/aster.img") do ds
+    # get image size
+    rows = GU.height(ds)
+    cols = GU.width(ds)
+    bands = GU.nraster(ds)
+
+    # get the band and block sizes
+    band = GU.fetchband(ds, 1)
+    (xbsize, ybsize) = GU.getblocksize(band)
+
+    # initialize variables
+    count = 0
+    total = 0
+
+    data = GU.fetch(ds, 1)
+    for i in 1:ybsize:rows
+        # loop through the columns
+        for j in 1:xbsize:cols
+            if i + ybsize < rows
+                nrows = ybsize
+            else
+                nrows = rows - (i-1)
+            end
+            if j + xbsize < cols
+                ncols = xbsize
+            else
+                ncols = cols - (j-1)
+            end
+            window = data[j:j+ncols-1, i:i+nrows-1]
+            count = count + sum(window .> 0)
+            total = total + sum(window)
+        end
+    end
+    println("Ignoring 0:  $(total / count)")
+    println("Including 0: $(total / (rows * cols))")
+end
+
+# Ignoring 0:  76.33891347095299
+# Including 0: 47.55674749653172
+#   0.250603 seconds (154.79 k allocations: 90.796 MB, 31.42% gc time)
+
+import GDALUtils; const GU = GDALUtils
+
+# version 2
+@time GU.read("ospy/data4/aster.img") do ds
+    # get image size
+    rows = GU.height(ds)
+    cols = GU.width(ds)
+    bands = GU.nraster(ds)
+
+    # get the band and block sizes
+    band = GU.fetchband(ds, 1)
+    (xbsize, ybsize) = GU.getblocksize(band)
+
+    # initialize variables
+    count = 0
+    total = 0
+
+    buffer = Array(GU.getdatatype(band), ybsize, xbsize)
+    # loop through the rows
+    for i in 0:ybsize:(rows-1)
+        # loop through the columns
+        for j in 0:xbsize:(cols-1)
+            if i + ybsize < rows
+                nrows = ybsize
+            else
+                nrows = rows - i
+            end
+            if j + xbsize < cols
+                ncols = xbsize
+            else
+                ncols = cols - j
+            end
+            GU.rasterio!(band, buffer, ncols, nrows, j, i)
+            data = buffer[1:nrows,1:ncols]
+            count += sum(data .> 0)
+            total += sum(data)
+        end
+    end
+
+    # print results
+    println("Ignoring 0:  $(total / count)")
+    println("Including 0: $(total / (rows * cols))")
+end
+
+# Ignoring 0:  76.33891347095299
+# Including 0: 47.55674749653172
+#   0.184718 seconds (150.16 k allocations: 63.625 MB, 9.85% gc time)
